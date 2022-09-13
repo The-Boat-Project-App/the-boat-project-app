ecbf316672b50a31ad7ad0539aba6c0721a1a693
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2022-09-13 23:15:30 +0200

    setup Redux Toolkit & MapView

--- mobile/navigation/Tabs.tsx
+++ mobile/navigation/Tabs.tsx
@@ -5,6 +5,8 @@ import { BottomTabNavigatorParamList } from './types'
 import HomeStackNavigator from './HomeStack'
 import HomeScreen from '../screens/HomeScreen/HomeScreen'
 import MapScreen from '../screens/MapScreen/MapScreen'
+import OtherScreen from '../screens/OtherScreen/OtherScreen'
+import CarteScreen from '../screens/CarteScreen/CarteScreen'
 
 const Tab = createBottomTabNavigator()
 
@@ -17,6 +19,8 @@ const BottomTabs = () => {
     >
       <Tab.Screen name='Home' component={HomeScreen} />
       <Tab.Screen name='Map' component={MapScreen} />
+      <Tab.Screen name='Other' component={OtherScreen} />
+      <Tab.Screen name='Carte' component={CarteScreen} />
     </Tab.Navigator>
   )
 }
